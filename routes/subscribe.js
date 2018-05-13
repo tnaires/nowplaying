d91a518151cab0d8bceec418ee9c27ee778ad39e
Tarso Aires <tnaires@gmail.com>
2018-05-13 13:34:16 -0300

    Extract Twitter connection code

--- routes/subscribe.js
+++ routes/subscribe.js
@@ -1,25 +1,13 @@
 const express = require('express');
-const Twitter = require('twitter');
+
+const openTweetsFilterStream = require('../services/twitter');
 
 module.exports = (io) => {
   const router = express.Router();
 
   router.get('/', (req, res, next) => {
-    const client = new Twitter({
-      consumer_key: process.env.TWITTER_CONSUMER_KEY,
-      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-      access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-      access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-    });
-
-    client.stream('statuses/filter', {track: 'nowplaying'}, stream => {
-      stream.on('data', tweet => {
-        console.log(tweet);
-      });
-
-      stream.on('error', error => {
-        throw error;
-      });
+    openTweetsFilterStream('nowplaying', tweet => {
+      console.log(tweet);
     });
 
     res.send('Subscribed');
