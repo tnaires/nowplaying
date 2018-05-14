f5233df4cda6d26b29429dad57dfd5bf7c4d6910
Tarso Aires <tnaires@gmail.com>
2018-05-13 22:44:50 -0300

    Refactor componentWillReceiveProps event within LocationFilter component

--- client/src/components/LocationFilter.js
+++ client/src/components/LocationFilter.js
@@ -10,25 +10,34 @@ class LocationFilter extends Component {
     message: 'Fetching location data...'
   };
 
+  extractCityNameFrom(data) {
+    return data[0]
+      .address_components
+      .filter(item => item.types.includes(RESULT_TYPE))[0]
+      .short_name;
+  }
+
+  extractLocationFrom(data) {
+    const bounds = data[0].geometry.bounds;
+    const swLng = bounds.southwest.lng;
+    const swLat = bounds.southwest.lat;
+    const neLng = bounds.northeast.lng;
+    const neLat = bounds.northeast.lat;
+
+    return `${swLng},${swLat},${neLng},${neLat}`;
+  }
+
   componentWillReceiveProps(nextProps) {
     if (nextProps.coords) {
       fetch(`/geocode/reverse?latitude=${nextProps.coords.latitude}&longitude=${nextProps.coords.longitude}&result_type=${RESULT_TYPE}`)
       .then(response => {
         response.json().then(data => {
-          const cityName = data[0]
-            .address_components
-            .filter(item => item.types.includes(RESULT_TYPE))[0]
-            .short_name;
-
-          const bounds = data[0].geometry.bounds;
-          const swLng = bounds.southwest.lng;
-          const swLat = bounds.southwest.lat;
-          const neLng = bounds.northeast.lng;
-          const neLat = bounds.northeast.lat;
+          const cityName = this.extractCityNameFrom(data);
+          const location = this.extractLocationFrom(data);
 
           this.setState({
             message: `Fetching tweets from ${cityName}...`,
-            location: `${swLng},${swLat},${neLng},${neLat}`
+            location
           });
         });
       });
